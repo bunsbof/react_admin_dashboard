91c6434c249bfaa7ec4ec8d09fdb652d5789193f
bunsbof <nguyenminhcuong2000@gmail.com>
2022-07-28 22:13:03 +0700

    #2022/07/28 cuong - Handle Click Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ import {BsChatLeft} from 'react-icons/bs';
 import {RiNotification3Line} from 'react-icons/ri';
 import {MdKeyboardArrowDown} from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import {avatar} from '../data/avatar.jpg';
+import {avatar} from '../data/avatar2.jpg';
 import {Cart, Chat, Notification, UserProfile} from '.';
 import { useStateContext } from '../contexts/ContextProvider';
+
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
   <TooltipComponent content={title} position="BottomCenter">
@@ -20,7 +21,7 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 )
 
 const Navbar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, isClicked, setIsClicked} = useStateContext();
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
@@ -32,13 +33,21 @@ const Navbar = () => {
       <NavButton title="Notification" dotColor="#03C9D7" customFunc={() => handleClick('notification')} color="blue" icon={<RiNotification3Line/>}/>
       <TooltipComponent content="Profile" position='BottomCenter'>
         <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray grounded-lg" onClick={() => handleClick('userProfile')}>
-          <img src="{avatar}" className='rounded-full w-8 h-8' />
+          <img src={avatar} className='rounded-full w-8 h-8' alt=''/>
           <p>
             <span className='text-gray-400 text-14'>Hi,</span> {' '}
             <span className='text-gray-400 font-bold ml-1 text-14'>Bunsbof</span>
           </p>
+          <MdKeyboardArrowDown
+            className='text-gray-400 text-14'
+          />
         </div>
       </TooltipComponent>
+      {isClicked.cart && <Cart/>}
+      {isClicked.chat && <Chat/>}
+      {isClicked.notification && <Notification/>}
+      {isClicked.userProfile && <UserProfile/>}
+
       </div>
       
     </div>
