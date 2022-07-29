83ec55c4de96f98662d65bc2fd8dcd20afc45291
bunsbof <nguyenminhcuong2000@gmail.com>
2022-07-29 22:42:37 +0700

    #2022/07/29 cuong - Sparkline Imported

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
-import { SiShopware } from 'react-icons/si';
-import { MdOutlineCancel } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import { links } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) setActiveMenu(false);
+  };
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
-    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between item-center">
-            <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <Link
+              to="/"
+              onClick={handleCloseSideBar}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
               <SiShopware /> <span>Shoppy</span>
             </Link>
-            <TooltipComponent content="Menu" position='BottomCenter'>
-              <button type='button' onClick={() => setActiveMenu((preActiveMenu)=> !preActiveMenu)} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block">
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => setActiveMenu((preActiveMenu) => !preActiveMenu)}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
+              >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
@@ -29,11 +42,16 @@ const Sidebar = () => {
               <div key={item.title}>
                 <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
                 {item.links.map((link) => (
-                  <NavLink to={`/${link.name}`} key={`/${link.name}`} onClick={() => { }} className={({ isActive }) => isActive ? activeLink : normalLink}>
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={`/${link.name}`}
+                    onClick={handleCloseSideBar}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
                     {link.icon}
-                    <span className='capitalize'>
-                      {link.name}
-                    </span>
+                    <span className="capitalize">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
@@ -45,4 +63,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar
+export default Sidebar;
